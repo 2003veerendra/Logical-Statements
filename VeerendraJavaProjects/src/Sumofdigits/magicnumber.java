889af8f78b242f4fa908a9c66e52983f6e74d2e9
2003veerendra <veerendrasaithana@gmail.com>
2026-08-14 10:46:49 +0530

    Magic Number Prgm

---
+++ VeerendraJavaProjects/src/Sumofdigits/magicnumber.java
@@ -0,0 +1,28 @@
+package Sumofdigits;
+
+import java.util.Scanner;
+
+public class magicnumber {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner (System.in)) {
+			System.out.println("Enter a number : ");
+			int n= sc.nextInt();
+			while (n>9) {
+				int sum = 0;
+				while (n>0) {
+					int digit = n % 10;
+					sum = sum + digit;
+					n = n/10;
+				}
+				n = sum;
+			}
+			if (n==1)
+System.out.println("Magic Number");
+			else
+				System.out.println("Not a magic number");
+		}
+		}
+
+	}
+
